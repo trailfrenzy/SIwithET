29ce584fb6d8c7407e5ad24d218556656f5052dd
Curt <31280803+CurtMartin@users.noreply.github.com>
2018-08-10 10:55:47 -0500

    Added comment

--- include/template_help.h
+++ include/template_help.h
@@ -1,21 +1,20 @@
+/** These helper structures for compile time branching were provided in the Boost libraries but did not want to include Boost in my build if only these features were required.
+*/
 #ifndef UNIT_TYPE_TEMPLATE_HELPER_INCLUDE_H_
 #define UNIT_TYPE_TEMPLATE_HELPER_INCLUDE_H_
 #pragma warning( disable : 4091 )  
 
 namespace SystemOfUnits
 {
-
       /** \brief The struct is from "Generative Programming", pg 409 
 	  */
 	  template<bool condition, typename THEN, typename ELSE> struct IF
 	  {
-		  //typedef THEN RET;
 		  using RET = THEN;
 	  };
 
 	  template<typename THEN, typename ELSE>struct IF<false, THEN, ELSE >
 	  {
-		  //typedef ELSE RET;
 		  using RET = ELSE;
 	  };
 
