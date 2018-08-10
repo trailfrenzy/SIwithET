5cdd7438387e7290a266ef6215c57a0aee3e920b
Curt <31280803+CurtMartin@users.noreply.github.com>
2018-08-10 11:50:00 -0500

    Clean code
    Format the code and remove commented out code.

--- test/EnglishUnitTest.cpp
+++ test/EnglishUnitTest.cpp
@@ -1,5 +1,4 @@
 #include <gtest\gtest.h>
-//#include <cppunit/extensions/HelperMacros.h>
 #include "EnglishTypes.h"
 #include "MetricTypes.h"
 #include "operators.h"
@@ -10,56 +9,36 @@ namespace
    constexpr double delta = 0.000001;
 }
 
-// basic test on the multiplication operator
-//class EnglishUnitTest : public ::testing::Test //: public CppUnit::TestFixture
-//{
-//public:
-//	void SetUp() {}
-//	void TearDown() {}
-//private:
-//}
-   /// Test converting Feet per second from Feet / Second.
-   TEST(EnglishUnitTest, TestFPS )
-   {
-      English::t_foot Feet = 144;
-      English::t_second Second = 4;
+/// Test converting Feet per second from Feet / Second.
+TEST(EnglishUnitTest, TestFPS)
+{
+	English::t_foot Feet = 144;
+	English::t_second Second = 4;
 
-      English::t_FPS fps = Feet / Second;
+	English::t_FPS fps = Feet / Second;
 
-      EXPECT_DOUBLE_EQ( 36.0, fps.amount() );
+	EXPECT_DOUBLE_EQ(36.0, fps.amount());
 
-      English::t_FPSS fpss = fps/English::t_second(2.0);
-	  EXPECT_DOUBLE_EQ( 18.0, fpss.amount() );
-   }
-   /// Tests converting different types of length.
-   TEST(EnglishUnitTest, TestLength )
-   {
-      using namespace SOU;
+	English::t_FPSS fpss = fps / English::t_second(2.0);
+	EXPECT_DOUBLE_EQ(18.0, fpss.amount());
+}
+/// Tests converting different types of length.
+TEST(EnglishUnitTest, TestLength)
+{
+	using namespace SOU;
 
-      English::t_mile mile = 1.0;
-      English::t_foot feet = SOU::conversion_cast< English::t_foot >( mile );
-	  EXPECT_DOUBLE_EQ( 5280.0, feet.amount() );
+	English::t_mile mile = 1.0;
+	English::t_foot feet = SOU::conversion_cast<English::t_foot>(mile);
+	EXPECT_DOUBLE_EQ(5280.0, feet.amount());
 
-      feet = 15;
-      English::t_yard yard = conversion_cast< English::t_yard >( feet );
-	  EXPECT_DOUBLE_EQ( 5.0, yard.amount() );
+	feet = 15;
+	English::t_yard yard = conversion_cast<English::t_yard>(feet);
+	EXPECT_DOUBLE_EQ(5.0, yard.amount());
 
-      English::t_mph mph = 65.0;
-      English::t_FPS fps = SOU::conversion_cast< English::t_FPS >( mph );
-	  EXPECT_NEAR( 95.333333333, fps.amount(), delta );
-   }
-
-//   CPPUNIT_TEST_SUITE( EnglishUnitTest );
-//   CPPUNIT_TEST( TestFPS );
-//   CPPUNIT_TEST( TestLength);
-//   //CPPUNIT_TEST( JustTest );
-//   //CPPUNIT_TEST( TestWithlog );
-//   CPPUNIT_TEST_SUITE_END();
-//
-//};
-//
-//
-//CPPUNIT_TEST_SUITE_REGISTRATION( EnglishUnitTest );
+	English::t_mph mph = 65.0;
+	English::t_FPS fps = SOU::conversion_cast<English::t_FPS>(mph);
+	EXPECT_NEAR(95.333333333, fps.amount(), delta);
+}
 
 // Copyright © 2005-2015 "Curt" Leslie L. Martin, All rights reserved.
 // curt.leslie.lewis.martin@gmail.com
