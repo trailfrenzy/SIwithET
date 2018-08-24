b2a9336a5b8244ac60fac9e78e42bdebdb0eeb03
Curt <31280803+CurtMartin@users.noreply.github.com>
2018-08-24 12:13:18 -0500

    Added Joule Test
    Added the Gravity Constant too.

--- test/JouleTest.cpp
+++ test/JouleTest.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include "MetricTypes.h"
 #include "WhatAmI.h"
+#include "operators.h"
 /*
 The joule is a derived unit of energy in the International System of Units. It is equal to the energy transferred to an object when a force of one newton acts on that object in the direction of its motion through a distance of one metre. Wikipedia
 SI base units: kg⋅m2⋅s−2
@@ -12,11 +13,13 @@ CGS units: 1×107 erg
 Named after: James Prescott Joule
 */
 
-
 //using t_Joule = Metric::AUMetric::MakeDim<2, -2, 1, 0, 0>::type;
 using t_MakeType = SOU::MakeType< Metric::AtomicUnit::Meter, AT::second, Metric::AtomicUnit::kilogram, Metric::AtomicUnit::kelvin, Metric::AtomicUnit::coulomb >;
 using t_Joule = t_MakeType::MakeDim<2, -2, 1, 0, 0>::type;
+using t_kilogram = t_MakeType::MakeDim<0, 0, 1, 0, 0>::type;
 
+using t_Grav = t_MakeType::MakeDim< 3, -2, -1, 0, 0 >::type;
+const t_Grav GRAVITY = 6.6740831e-11;
 
 TEST(Diminsion, Joule) {
 	t_Joule joule{ 45.033 };
@@ -36,4 +39,40 @@ TEST(Diminsion, Newton) {
 	t_Newton N{ 45.5885 };
 	EXPECT_EQ(SystemOfUnits::Diminsion(N), "L·t^-2·M");
 	EXPECT_EQ(SystemOfUnits::WhatAmI(N), "meter·second^-2·kilogram");
+}
+
+TEST(Joule, Product) {
+	t_Newton N{ 45.0 };
+	Metric::t_meter M{ 5.0 };
+	auto J = N * M;
+	EXPECT_EQ(SystemOfUnits::Diminsion(J), "L^2·t^-2·M");
+	EXPECT_EQ(SystemOfUnits::WhatAmI(J), "meter^2·second^-2·kilogram");
+	EXPECT_DOUBLE_EQ(225.0, J.amount());
+}
+
+TEST(Joule, Ratio) {
+	t_Joule J{ 45.0 };
+	t_Newton N{ 5.0 };
+	auto M = J / N;
+	EXPECT_DOUBLE_EQ(9.0, M.amount());
+	EXPECT_EQ(SystemOfUnits::WhatAmI(M), std::string("meter"));
+}
+using t_Meter = t_MakeType::MakeDim<1, 0, 0, 0, 0>::type;
+using t_MeterSq = t_MakeType::MakeDim<2, 0, 0, 0, 0>::type;
+
+TEST(Joule, Pressure) {
+	t_Newton N{ 45.0 };
+	t_MeterSq M2 = 9.0;
+	auto Pressure = N / M2;
+	EXPECT_DOUBLE_EQ(5.0, Pressure.amount()) << "Building up for the ideal gas law"; // kg/(m·s2)
+	//EXPECT_EQ( SystemOfUnits::WhatAmI( P ), "")
+}
+
+TEST(Joule, Gravity) {
+	t_kilogram const M1 = 5.0;
+	t_kilogram const M2 = 3.0;
+	t_Meter const r = 10.0;
+
+	auto F = GRAVITY * M1 * M2 / (r*r);
+	EXPECT_EQ(SystemOfUnits::Diminsion(F), SystemOfUnits::Diminsion(t_Newton(2.0)));
 }
