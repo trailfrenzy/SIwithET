94e1c46a3148c6f9292e2e92cde553d7a9770c88
Curt <trailfrenzy@gmail.com>
2020-11-08 08:55:41 -0800

    Inclose the MACRO in a inline function
    Allows the compiler to choose wheather to inline the call or make it a function call. Prior to this the complier was not given the oppurtunity with the macro. Will need to re-examine this so output statements are allowed when a failure.

--- test/ExpectUnitTest.h
+++ test/ExpectUnitTest.h
@@ -7,26 +7,52 @@
 #include "SI.h"
 #include <gtest/gtest.h>
 
-template< SOU::UnitSerial T, SOU::UnitSerial U > auto EXPECT_UNIT_EQ(T t, U u)
+/// <summary>
+/// Wraps the macro EXPECT_PRED_FORMAT2 in function which can be called over again. Easier for the compiler to decide to inline it or call it.
+/// </summary>
+/// <param name="t"></param>
+/// <param name="u"></param>
+/// <returns></returns>
+inline auto TheMacro(double t, double u)
+{
+   EXPECT_PRED_FORMAT2(::testing::internal::CmpHelperFloatingPointEQ<double>, t, u);
+}
+
+template< SOU::UnitSpecies T, SOU::UnitSpecies U > auto EXPECT_UNIT_EQ(T t, U u)
 {
    static_assert(std::is_same< T, U>::value, "These two types must be the same, if not do not compile");
 
-   EXPECT_PRED_FORMAT2(::testing::internal::CmpHelperFloatingPointEQ<double>, t.amount(), u.amount() );
+   TheMacro(t.amount(), u.amount());
 }
 
-template< SOU::UnitSerial T > auto EXPECT_UNIT_EQ(T t, double u)
+template< SOU::UnitSpecies T > auto EXPECT_UNIT_EQ(T t, double u)
 {
    // TODO Set up for return type someday.
    /* return *//* did not work will return some other day. */
-   EXPECT_PRED_FORMAT2(::testing::internal::CmpHelperFloatingPointEQ<double>, t.amount(), u);
+   TheMacro(t.amount(), u);
 }
 
-template< SOU::UnitSerial U > auto EXPECT_UNIT_EQ(double t, U u)
+template< SOU::UnitSpecies U > auto EXPECT_UNIT_EQ(double t, U u)
 {
-   EXPECT_PRED_FORMAT2(::testing::internal::CmpHelperFloatingPointEQ<double>, t, u.amount());
+   TheMacro(t, u.amount());
 }
 
 #endif
+//template< typename T, typename U > struct IsBothSOU
+//{
+//   constexpr static bool value = SOU::is_UnitType<T>::value && SOU::is_UnitType<U>::value;
+//};
+//
+//#define EXPECT_UNIT_EQ( t, u ) \
+//   if constexpr( IsBothSOU<decltype(t), decltype(u) >::value ) { \
+//      static_assert(std::is_same< decltype(t), decltype(u)>::value, "These two types must be the same, if not do not compile"); \
+//      EXPECT_PRED_FORMAT2(::testing::internal::CmpHelperFloatingPointEQ<double>, t.amount(), u.amount()); \
+//   } \
+//   else if constexpr( SOU::is_UnitType<decltype(t)>::value ) \
+//      EXPECT_PRED_FORMAT2(::testing::internal::CmpHelperFloatingPointEQ<double>, t.amount(), u); \
+//   else if constexpr( SOU::is_UnitType<decltype(u)>::value ) \
+//      EXPECT_PRED_FORMAT2(::testing::internal::CmpHelperFloatingPointEQ<double>, t, u.amount()); \
+//   else {static_assert(false, "Non-UnitTypes where passes as arguments"); }
 
 // Copyright © 2005-2020 "Curt" Leslie L. Martin, All rights reserved.
 // curt.leslie.lewis.martin@gmail.com
